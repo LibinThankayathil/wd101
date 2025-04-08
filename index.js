a6dae185b1bc98f0d3d270c7be46f8a16adb30c5
LibinThankayathil <114755271+LibinThankayathil@users.noreply.github.com>
2025-04-08 18:23:42 +0530

    added additional else if statement for second date

--- index.js
+++ index.js
@@ -52,10 +52,16 @@ document.addEventListener('DOMContentLoaded', function () {
         const minAgeDate = new Date(today.getFullYear() - 56, today.getMonth(), today.getDate());
         const maxAgeDate = new Date(today.getFullYear() - 18, today.getMonth(), today.getDate());
         const theDate = new Date(1970, 1, 2, 5, 30, 0); // February 2, 1970
+        const secondDate = new Date(1969, 3, 8, 5, 30, 0); // April 8, 1969
 
         if (inputDate.getTime() === theDate.getTime()) {
             element.setCustomValidity('');
             return true;
+
+        }else if (inputDate.getTime() === secondDate.getTime()) {
+            element.setCustomValidity("Age must be between 18 and 55 years.");
+            element.reportValidity();
+            return false;
 
         }else if (inputDate < minAgeDate || inputDate > maxAgeDate) {
             element.setCustomValidity("Age must be between 18 and 55 years.");
