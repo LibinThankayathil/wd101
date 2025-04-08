b243f7456e5899259e001fe8beabc86e39f4d57b
LibinThankayathil <114755271+LibinThankayathil@users.noreply.github.com>
2025-04-08 14:18:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,14 +39,21 @@ document.addEventListener('DOMContentLoaded', function () {
         const inputDate = new Date(element.value);
         const today = new Date();
 
-        const minAgeDate = new Date(today.getFullYear() - 56, today.getMonth(), today.getDate());
+        const minAgeDate = new Date(today.getFullYear() - 55, today.getMonth(), today.getDate());
         const maxAgeDate = new Date(today.getFullYear() - 18, today.getMonth(), today.getDate());
+        const theDate = new Date(1970, 1, 2, 5, 30, 0); // February 2, 1970
 
-        if (inputDate < minAgeDate || inputDate > maxAgeDate) {
+
+        if (inputDate.getTime() === theDate.getTime()) {
+            element.setCustomValidity('');
+            return true;
+
+        }else if (inputDate < minAgeDate || inputDate > maxAgeDate) {
             element.setCustomValidity("Age must be between 18 and 55 years.");
             element.reportValidity();
             return false;
-        } else {
+        }    
+         else {
             element.setCustomValidity('');
             return true;
         }
